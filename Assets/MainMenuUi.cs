fea085c581fefb3870f449d85f8a8106805be8f7
redadoo <edoardovocaturo@gmail.com>
2024-01-28 08:12:35 +0100

    add video for main menu

--- Assets/MainMenuUi.cs
+++ Assets/MainMenuUi.cs
@@ -4,16 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainMenuUi : MonoBehaviour
 {
-    [SerializeField] private Button playButton;
-    [SerializeField] private Button quitButton;
+    [SerializeField] private Button         playButton;
+    [SerializeField] private Button         quitButton;
+    [SerializeField] private VideoPlayer    videoPlayer;
+    [SerializeField] private RawImage       raw;
 
 
     private void Start()
     {
         playButton.onClick.AddListener(() => SceneManager.LoadScene("MAIN"));
         quitButton.onClick.AddListener(() => Application.Quit());
+
+        videoPlayer.loopPointReached += VideoPlayer_loopPointReached;
+        videoPlayer.started += VideoPlayer_started; ;
+
     }
+
+    private void VideoPlayer_started(VideoPlayer source)
+    {
+        playButton.interactable = false;
+        quitButton.interactable = false;
+    }
+
+    private void VideoPlayer_loopPointReached(VideoPlayer source)
+    {
+        Destroy(raw);
+        playButton.interactable = true;
+        quitButton.interactable = true;
+    }
+
+
+
 }
